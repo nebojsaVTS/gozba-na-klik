86581d825eec6ee99985e3d78e7efe67c7becc23
Nebojsa Nedeljkovic <neso.1995@hotmail.com>
2026-05-15 01:49:57 +0200

    Configure EF Core PostgreSql setup

---
+++ backend/GozbaNaKlik.API/Data/AppDbConetext.cs
@@ -0,0 +1,13 @@
+﻿using GozbaNaKlik.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GozbaNaKlik.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
