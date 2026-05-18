5d0839f6625f4f75be03e1c0ac1b67f4aa1a1ed1
Dejan <dejanpesut.dv@gmail.com>
2026-05-18 19:30:12 +0200

    Register forme jsx

---
+++ frontend/gozba-na-klik-client/src/components/Register.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "/Register.css";
+
+function Register() {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        email: "",
+    });
+
+    const [message, setMessage] = useState("");
+
+    function handleChange(event) {
+        setFormData ({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if(
+            !formData.username ||
+            !formData.password ||
+            !formData.email
+        ){
+            setMessage("Sva polja moraju biti popunjena.");
+            return;
+        }
+
+        setMessage("Uspesna registracija!")
+
+        setFormData({
+            username: "",
+            password: "",
+            email: "",
+        });
+    }
+
+
+    return (
+        <div className="register-container">
+            <h2>Registracija</h2>
+
+            <from onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+
+                <input 
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                />
+
+                <button type="submit">Registruj se</button>
+            </from>
+
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
+
+export default Register;
