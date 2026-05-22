8f9939f0ee024146c61413f65ce63f29aaf9f7bd
Dejan <dejanpesut.dv@gmail.com>
2026-05-22 09:17:10 +0200

    ispravljene greske za WelcomePage,App.jsx, Nakon registracije prebaci na Login

--- frontend/gozba-na-klik-client/src/components/WelcomePage.jsx
+++ frontend/gozba-na-klik-client/src/components/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import "./WelcomePage.scss";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const WelcomePage = () => {
   const navigate = useNavigate();
