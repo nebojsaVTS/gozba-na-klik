30d4fc8e16d42698cc4fbf7c78903d30a980fa87
Nikola Subotic <nikola.subotic.0303@gmail.com>
2026-06-05 11:45:06 +0200

    Update admin users page

--- frontend/gozba-na-klik-client/src/components/AdminUsers.jsx
+++ frontend/gozba-na-klik-client/src/components/AdminUsers.jsx
@@ -1,6 +1,17 @@
 import "./AdminUsers.scss";
+import { useState } from "react";
 
 function AdminUsers() {
+
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+   const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    role: ""
+  });
+
   const users = [
     {
       username: "admin1",
@@ -23,10 +34,65 @@ function AdminUsers() {
       role: "Vlasnik restorana"
     }
   ];
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      !formData.username ||
+      !formData.password ||
+      !formData.email ||
+      !formData.role
+    ) {
+      alert("Sva polja su obavezna");
+      return;
+    }
+
+    console.log("Novi korisnik:", formData);
+
+    // ovde kasnije ide API call
+
+    setFormData({
+      username: "",
+      password: "",
+      email: "",
+      role: ""
+    });
+
+    setShowRegisterForm(false);
+  };
+  
   return (
     <div className="admin-users-container">
       <h2>Pregled registrovanih korisnika</h2>
+
+      <button onClick={() => setShowRegisterForm(true)}>
+          Registruj novog korisnika
+      </button>
+
+      {showRegisterForm && (
+  <form>
+    <input type="text" placeholder="Korisničko ime" />
+    <input type="password" placeholder="Password" />
+    <input type="email" placeholder="Email" />
+
+    <select>
+      <option value="">Izaberi ulogu</option>
+      <option value="Courier">Kurir</option>
+      <option value="RestaurantOwner">Vlasnik restorana</option>
+    </select>
+
+    <button type="submit">Registruj</button>
+  </form>
+)}
 
       <table className="users-table">
         <thead>
