dee7deb5aaf00f055772374832006e08342f4f23
Nikola Subotic <nikola.subotic.0303@gmail.com>
2026-07-03 01:05:41 +0200

    Complete restaurant creation logic

--- backend/GozbaNaKlik.API/DTOs/CreateRestaurantDto.cs
+++ backend/GozbaNaKlik.API/DTOs/CreateRestaurantDto.cs
@@ -1,6 +1,14 @@
 ﻿namespace GozbaNaKlik.API.DTOs
+    
 {
-    public class CreateRestaurantDto
-    {
+        public class CreateRestaurantDto
+        {
+            public string Name { get; set; }
+
+            public string Address { get; set; }
+
+            public string PhoneNumber { get; set; }
+
+            public int OwnerId { get; set; }
+        }
     }
-}
