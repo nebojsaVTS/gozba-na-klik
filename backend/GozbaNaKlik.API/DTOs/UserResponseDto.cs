b77b64da8ec9551b9c4f04ea4bfbf47ff9505f31
Nikola Subotic <nikola.subotic.0303@gmail.com>
2026-07-03 02:18:47 +0200

    Add endpoint for listing registered users

---
+++ backend/GozbaNaKlik.API/DTOs/UserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace GozbaNaKlik.API.DTOs
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+    }
+}
